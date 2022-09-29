917f306f37f65fd4f54e914417573a25e0046605
kbouda <102676776+kbouda@users.noreply.github.com>
2022-09-29 14:59:42 -0400

    push 3 first shapes

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,3 +1,38 @@
-document.addEventListener('DOMContentLoaded', () ) => {
-    
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  const squares = Array.from(document.querySelectorAll('.grid div'))
+  const scoreDisplay = document.querySelector('#score')
+  const StartBtn = document.querySelector('#start_button')
+  const width = 10
+
+  //Tetrominoes
+  const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
+  const zTetromino = [
+    [0, width, width+1, width*2+1]
+    [width+1, width+2, width*2, width*2+1]
+    [0, width, width+1, width*2+1]
+    [width+1, width+2, width*2, width*2+1]
+
+  ]
+
+  const tTeromino = [
+    [1, width, width+1, width+2]
+    [1, width+1, width+2, width*2+1]
+    [width, width+1, width+2, width*2+1]
+    [1, width, width+1, width*2+1]
+
+  ]
+
+  const 
+
+
+
+
+
+})
