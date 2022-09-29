40f8c676fc6cd493ec4b656f5dd25d6ca6f1d1ad
kbouda <102676776+kbouda@users.noreply.github.com>
2022-09-29 16:58:42 -0400

    freezing tetrominoes

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   ];
 
-  const tTeromino = [
+  const tTetromino = [
     [1, width, width + 1, width + 2],
     [1, width + 1, width + 2, width * 2 + 1],
     [width, width + 1, width + 2, width * 2 + 1],
@@ -43,6 +43,50 @@ document.addEventListener('DOMContentLoaded', () => {
     [width, width + 1, width + 2, width + 3]
   ];
 
-  const theTetrominoes = [lTetromino, zTetromino, tTeromino, oTetromino, iTetromino];
-  
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+  let currentPosition = 4;
+  const currentRotation = 0;
+
+  // randomly select a Tetromino and its first rotation
+  let random = Math.floor(Math.random() * theTetrominoes.length);
+  let current = theTetrominoes[random][currentRotation];
+
+  // draw the  tretromino
+  function draw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.add('tetromino');
+    });
+  }
+
+  function undraw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.remove('tetromino');
+    });
+  }
+
+  // make tetromino move down every second
+  // eslint-disable-next-line no-use-before-define
+  timerId = setInterval(moveDown, 1000);
+
+  // move down function
+  function moveDown() {
+    undraw();
+    currentPosition += width;
+    draw();
+    // eslint-disable-next-line no-use-before-define
+    freeze();
+  }
+
+  // freeze function
+  function freeze() {
+    if (current.some((index) => squares[currentPosition + index + width].classList.contains('taken'))) {
+      current.forEach((index) => squares[currentPosition + index].classList.add('taken'));
+      // start new tetromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
+  }
 });
