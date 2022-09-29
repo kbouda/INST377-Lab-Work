4524ca15daec30d192973ca5f9ac06d019ece7d3
kbouda <102676776+kbouda@users.noreply.github.com>
2022-09-29 15:05:58 -0400

    added all shapes

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,38 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const grid = document.querySelector('.grid')
-  const squares = Array.from(document.querySelectorAll('.grid div'))
-  const scoreDisplay = document.querySelector('#score')
-  const StartBtn = document.querySelector('#start_button')
-  const width = 10
+  const grid = document.querySelector('.grid');
+  const squares = Array.from(document.querySelectorAll('.grid div'));
+  const scoreDisplay = document.querySelector('#score');
+  const StartBtn = document.querySelector('#start_button');
+  const width = 10;
 
-  //Tetrominoes
+  // Tetrominoes
   const lTetromino = [
-    [1, width+1, width*2+1, 2],
-    [width, width+1, width+2, width*2+2],
-    [1, width+1, width*2+1, width*2],
-    [width, width*2, width*2+1, width*2+2]
-  ]
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
+  ];
 
   const zTetromino = [
-    [0, width, width+1, width*2+1]
-    [width+1, width+2, width*2, width*2+1]
-    [0, width, width+1, width*2+1]
-    [width+1, width+2, width*2, width*2+1]
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1]
 
-  ]
+  ];
 
   const tTeromino = [
-    [1, width, width+1, width+2]
-    [1, width+1, width+2, width*2+1]
-    [width, width+1, width+2, width*2+1]
-    [1, width, width+1, width*2+1]
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
 
-  ]
+  ];
 
-  const 
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ];
 
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ];
 
-
-
-
-})
+  const theTetrominoes = [lTetromino, zTetromino, tTeromino, oTetromino, iTetromino];
+  
+});
