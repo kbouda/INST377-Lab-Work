ccf1ac0bf15a7fd6bd88eab9b5b775b07d00ec68
kbouda <102676776+kbouda@users.noreply.github.com>
2022-12-04 15:42:13 -0500

    fixed

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,16 +1,9 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable max-len */
 
-/*
-  ## Utility Functions
-    Under this comment place any utility functions you need - like an inclusive random number selector
-    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-*/
 function getRandomIntInclusive(min, max) {
   // eslint-disable-next-line no-param-reassign
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function injectHTML(list) {
